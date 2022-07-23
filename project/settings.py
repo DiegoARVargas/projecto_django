0963952f08db58fcd5f76aad0e05b038c9cb0f63
diego <rodriguezvargasd214@gmail.com>
2022-07-22 19:22:18 -0500

    Creando app polls, migrate de modelos y registro en admin

--- project/settings.py
+++ project/settings.py
@@ -22,8 +22,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+#print(f'BAse DIR: {BASE_DIR}')
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+#print('os.path.join', os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
 ]
 
 MIDDLEWARE = [
