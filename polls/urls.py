e1e5c04c885b3795c0e102dda50194071ecba010
diego <rodriguezvargasd214@gmail.com>
2022-08-01 14:48:49 -0500

    Demostrando como funciona render utilizando loader

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import index, primer_template
+from .views import index, primer_template, template_raw
 
 #Define la relacion entre una path y una view
 urlpatterns = [
     path('index/', index, name='index'),
-    path('template/', primer_template, name='primer_template')
+    path('template/', primer_template, name='primer_template'),
+    path('template02/', template_raw, name='segundo template')
 ]
