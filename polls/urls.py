a6524c5644b335fbf467815d043cf1392f32bea4
diego <rodriguezvargasd214@gmail.com>
2022-08-01 13:48:31 -0500

    Incorporamos templates al proyecto (templates/polls/index.html) y utlizamos el metodo render

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import index
+from .views import index, primer_template
 
 #Define la relacion entre una path y una view
 urlpatterns = [
-    path('index/', index, name='index')
+    path('index/', index, name='index'),
+    path('template/', primer_template, name='primer_template')
 ]
--- polls/views.py
+++ polls/views.py
@@ -8,3 +8,26 @@ from django.http import HttpResponse
 #signature: (request:Object --> HttpResponse)
 def index(request):
     return HttpResponse('Hola Django, esta es la vista de index.')
+
+def primer_template(request): # El template de Django es server rendering, es decir que se ejecuta en el servidor
+    kiko = 'valor'
+    valores = ('verde', 'azul', 'amarillo', 'rojo')
+    return render(
+        request, 
+        template_name='polls/index.html', 
+        context={'name': kiko, 'colores': valores}
+    )
+
+
+# Demostramos como funciona eñ shortcuts "render"
+from django.template import loader
+
+def template_raw(request):
+    kiko = 'valor'
+    valores = ('verde', 'azul', 'amarillo', 'rojo')
+    respuesta = loader.render_to_string(
+        request = request,
+        template_name='polls/index.html', 
+        context={'name': kiko, 'colores': valores}
+    )
+    return HttpResponse(respuesta)
