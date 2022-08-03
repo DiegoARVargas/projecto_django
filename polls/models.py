53de8c70e01cb4b656b3b86cab7e7cb8f30d5437
diego <rodriguezvargasd214@gmail.com>
2022-08-03 15:23:15 -0500

    Agregando una columna a la clase Deudor de la base de datos, luego creamos y aplicamos el migrate.

--- polls/models.py
+++ polls/models.py
@@ -5,12 +5,13 @@ from django.db import models
 
 # 1. Creas/Modificas/Eliminas un modelo (ORM)
 # 2. Crear una migracion: makemigrations <nombre_app>
-# 3. 
+# 3. Aplicas los cambios en la base de datos: migrate <nombre_app>
 
-class Deudor(models.Model):
+class Deudor(models.Model): #Django automaticamente define el nombre de la tabla como: <appanme>_<classname> todo en minuscula
     name = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     deuda = models.DecimalField(decimal_places=3, max_digits=50)
+    data_de_actualizacion = models.DateTimeField(auto_now=True)
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
