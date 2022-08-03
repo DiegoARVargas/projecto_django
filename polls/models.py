d54896b78ca4e84b739bbd6f9c908f558061665e
diego <rodriguezvargasd214@gmail.com>
2022-08-03 15:03:56 -0500

    Creando un nuevo modelo de nombre deudor y haciendo la migracion del mismo.

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,16 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here. - ORM: Object Relational Mapper
+
+# 1. Creas/Modificas/Eliminas un modelo (ORM)
+# 2. Crear una migracion: makemigrations <nombre_app>
+# 3. 
+
+class Deudor(models.Model):
+    name = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    deuda = models.DecimalField(decimal_places=3, max_digits=50)
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
