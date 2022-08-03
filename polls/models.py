5b0b990a351f96d17ccd0820db37ad79e28723f2
diego <rodriguezvargasd214@gmail.com>
2022-08-03 15:58:51 -0500

    Ingresando registros a la tabla/modelo Deudor a travez de la python shell}

--- polls/models.py
+++ polls/models.py
@@ -7,11 +7,18 @@ from django.db import models
 # 2. Crear una migracion: makemigrations <nombre_app>
 # 3. Aplicas los cambios en la base de datos: migrate <nombre_app>
 
-class Deudor(models.Model): #Django automaticamente define el nombre de la tabla como: <appanme>_<classname> todo en minuscula
+# Django automaticamente define el nombre de la tabla como: <appanme>_<classname> todo en minuscula
+# 3. Las instancias definen los modelos y representan registros en la tabla.
+# 4. Posibilita la consulta en la base de datos atravez de un MANAGER, atributo, objects
+
+class Deudor(models.Model): 
     name = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     deuda = models.DecimalField(decimal_places=3, max_digits=50)
     data_de_actualizacion = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
